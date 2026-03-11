2504d68d6ec96570dcde48191e80a9b975993a4f
github-action-benchmark <github@users.noreply.github.com>
2026-03-11 14:46:44 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for a6961b829970f17d59eed452f15243e14931f975

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773240404637,
+  "repoUrl": "https://github.com/MaciekWUP/n8n-mcp",
+  "entries": {
+    "n8n-mcp Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "moszkowskimaciej@gmail.com",
+            "name": "MaciekWUP",
+            "username": "MaciekWUP"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a6961b829970f17d59eed452f15243e14931f975",
+          "message": "Create claude.yml",
+          "timestamp": "2026-03-11T15:44:22+01:00",
+          "tree_id": "e65ddf237e3492baf01bab3aef9cba3892e091a7",
+          "url": "https://github.com/MaciekWUP/n8n-mcp/commit/a6961b829970f17d59eed452f15243e14931f975"
+        },
+        "date": 1773240404354,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0136,
+            "range": "0.3096",
+            "unit": "ms",
+            "extra": "73341 ops/sec"
+          }
+        ]
+      }
+    ]
+  }
+}
